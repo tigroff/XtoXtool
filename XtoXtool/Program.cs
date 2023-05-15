4a3eaa2b47ecc4aa9881b083724686ec92eee536
tigroff <tigroff@gmail.com>
2023-05-15 16:32:49 +0300

    Initial commit

---
+++ XtoXtool/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using dBASE.NET;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Linq;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+//select COM object Microsoft Excel xx.x Object Library before compiling!
+namespace XtoXtool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string remotePath = @"\\helpdata-1\HR_OR\";
+            Console.WriteLine("XML to XLS converter for PFU v.1.0 © 2023 Ihor Holovko");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: xtoxtool <filename.xml>");
+                ErrorExit("Nothing to convert.");
+                return;
+            }
+
+            string filePath = Path.GetDirectoryName(args[0]);
+            string xml;
+            try
+            {
+                xml = File.ReadAllText(args[0]);
+            }
+            catch (Exception ex)
+            {
+                ErrorExit(ex.Message);
+                return;
+            }
+            Console.WriteLine("Parsing XML ...");
+            var table1 = xml.ParseXML<Table>();
+            List<Part> dbfList = new List<Part>();
+
+            string dBFfile = Path.Combine(remotePath, $"{DateTime.Now.Year}{DateTime.Now.Month:D2}{DateTime.Now.Day:D2}", "people.dbf");
+
+            Dbf dbf = new Dbf();
+            if (File.Exists(dBFfile))
+            {
+                dbf.Read(dBFfile);
+                Console.Write("Parsing DBF ");
+                int i = dbf.Records.Count;
+                i /= 20;
+                int j = 0;
+                foreach (DbfRecord record in dbf.Records)
+                {
+                    dbfList.Add(new Part()
+                    {
+                        Tabn = Int32.Parse(record.Data[0]?.ToString() ?? "0"),
+                        Icnum = record.Data[26]?.ToString() ?? "",
+                        Ceh = record.Data[11]?.ToString() ?? "",
+                    }
+                    );
+                    j++;
+                    if (j > i)
+                    {
+                        Console.Write(".");
+                        j = 0;
+                    }
+
+                }
+            }
+            else
+            {
+                ErrorExit($"DBF file not found. Please check {dBFfile}");
+                return;
+            }
+
+            Excel.Application xlApp = new Excel.Application();
+            if (xlApp == null)
+            {
+                ErrorExit("Excel not found!");
+                return;
+            }
+            Workbook xlWorkBook;
+            Worksheet xlWorkSheet;
+            Excel.Range aRange;
+            object misValue = Type.Missing;
+
+            Console.WriteLine();
+            Console.Write("Compiling XLS ");
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 2] = $"{DateTime.Now:dd.MM.yyyy}";
+            xlWorkSheet.Cells[1, 11] = "ПАТ \"Укртатнафта\"";
+            aRange = xlWorkSheet.get_Range("A2", "N2");
+            aRange.Merge();
+            aRange.Font.Bold = true;
+            aRange.Font.Size = 12;
+            aRange.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            xlWorkSheet.Cells[2, 1] = "Лікарняні листки тимчасової непрацездатності";
+
+            short firstRow = 4;
+
+            xlWorkSheet.Cells[firstRow, 1] = "Цех";
+            xlWorkSheet.Cells[firstRow, 2] = "Табельний номер";
+            xlWorkSheet.Cells[firstRow, 3] = "Прізвище";
+            xlWorkSheet.Cells[firstRow, 4] = "Ім'я";
+            xlWorkSheet.Cells[firstRow, 5] = "По-батькові";
+            xlWorkSheet.Cells[firstRow, 6] = "Номер ЛН";
+            xlWorkSheet.Cells[firstRow, 7] = "Дата відкриття";
+            xlWorkSheet.Cells[firstRow, 8] = "Дата закриття";
+            xlWorkSheet.Cells[firstRow, 9] = "ІПН";
+            xlWorkSheet.Cells[firstRow, 10] = "Статус ЛН";
+            xlWorkSheet.Cells[firstRow, 11] = "Порушення режиму";
+            xlWorkSheet.Cells[firstRow, 12] = "Порушення алко/нарко";
+            xlWorkSheet.Cells[firstRow, 13] = "Причина лікарняного";
+            xlWorkSheet.Cells[firstRow, 14] = "Назва медзакладу";
+
+            aRange = xlWorkSheet.get_Range("A" + firstRow, "N" + firstRow);
+            aRange.Font.Bold = true;
+            aRange.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            aRange.VerticalAlignment = XlVAlign.xlVAlignCenter;
+            aRange.WrapText = true;
+            aRange.AutoFilter(3);
+
+            firstRow = 5;
+
+            for (int i = 0; i < table1.Rows.Count(); i++)
+            {
+                int index = dbfList.FindIndex(t => t.Icnum == (table1.Rows[i].NP_NUMIDENT.ToString()));
+                if (index != -1)
+                {
+                    xlWorkSheet.Cells[i + firstRow, 1] = dbfList[index].Ceh;
+                    xlWorkSheet.Cells[i + firstRow, 2] = dbfList[index].Tabn;
+                }
+
+                xlWorkSheet.Cells[i + firstRow, 3] = table1.Rows[i].NP_SURNAME;
+                xlWorkSheet.Cells[i + firstRow, 4] = table1.Rows[i].NP_NAME;
+                xlWorkSheet.Cells[i + firstRow, 5] = table1.Rows[i].NP_PATRONYMIC;
+
+                xlWorkSheet.Cells[i + firstRow, 6] = table1.Rows[i].WIC_NUM;
+                xlWorkSheet.Cells[i + firstRow, 7] = table1.Rows[i].WIC_DT_BEGIN;
+                xlWorkSheet.Cells[i + firstRow, 8] = table1.Rows[i].WIC_DT_END;
+                xlWorkSheet.Cells[i + firstRow, 9] = table1.Rows[i].NP_NUMIDENT;
+
+                xlWorkSheet.Cells[i + firstRow, 10] = table1.Rows[i].WIC_STATUS == "P" ? "Готовий до сплати" : "Закритий";
+                xlWorkSheet.Cells[i + firstRow, 11] = table1.Rows[i].VIOLATION_EXTENSION == true ? "Є порушення" : "Немає";
+                xlWorkSheet.Cells[i + firstRow, 12] = table1.Rows[i].SIGN_ANLK_NARKOTIK_INTOXICATION == true ? "Є порушення" : "Немає";
+
+                xlWorkSheet.Cells[i + firstRow, 13] = table1.Rows[i].WIC_CD_Name;
+                xlWorkSheet.Cells[i + firstRow, 14] = table1.Rows[i].HOSPITAL_NAME;
+                Console.Write(".");
+            }
+
+            firstRow = 4;
+
+            aRange = xlWorkSheet.get_Range("A" + (firstRow), "N" + (table1.Rows.Count() + firstRow));
+            aRange.Borders.Color = Color.Black.ToArgb();
+            aRange.Columns.AutoFit();
+
+            xlWorkSheet.Cells[table1.Rows.Count() + 6, 8] = "Начальник ВК СУП _______________________ Маргарита ІЦЕНКО";
+
+            dBFfile = Path.Combine(filePath, $"Лікарняні листки {DateTime.Now:ddMMyyyy}");
+
+            if (File.Exists(dBFfile + ".xls"))
+            {
+                dBFfile += $"_{DateTime.Now:HH_mm_ss}.xls";
+            }
+            else dBFfile += ".xls";
+
+            Console.WriteLine();
+            Console.WriteLine($"Save to file {dBFfile}");
+
+            try
+            {
+                xlWorkBook.SaveAs(dBFfile, XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+
+            }
+            catch (Exception ex)
+            {
+                ErrorExit(ex.Message);
+                return;
+            }
+
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+            Console.WriteLine("Exiting.");
+
+        }
+
+        private static void ErrorExit(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XtoXtool/XML.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XtoXtool
+{
+    // Примечание. Для запуска созданного кода может потребоваться NET Framework версии 4.5 или более поздней версии и .NET Core или Standard версии 2.0 или более поздней.
+    /// <remarks/>
+    [Serializable()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(Namespace = "", IsNullable = false)]
+    public partial class Table
+    {
+
+        private TableRow[] rowsField;
+
+        /// <remarks/>
+        [XmlArrayItem("Row", IsNullable = false)]
+        public TableRow[] Rows
+        {
+            get
+            {
+                return this.rowsField;
+            }
+            set
+            {
+                this.rowsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [Serializable()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [XmlType(AnonymousType = true)]
+    public partial class TableRow
+    {
+
+        private string wIC_NUMField;
+
+        private uint wIC_CASE_NUMField;
+
+        private System.DateTime wIC_DT_BEGINField;
+
+        private System.DateTime wIC_DT_ENDField;
+
+        private string wIC_STATUSField;
+
+        private byte wIC_CDField;
+
+        private string wIC_CD_NameField;
+
+        private bool sIGN_ANLK_NARKOTIK_INTOXICATIONField;
+
+        private bool vIOLATION_EXTENSIONField;
+
+        private string hOSPITAL_NAMEField;
+
+        private uint hOSPITAL_CODEField;
+
+        private string nP_SURNAMEField;
+
+        private string nP_NAMEField;
+
+        private string nP_PATRONYMICField;
+
+        private uint nP_NUMIDENTField;
+
+        private string nP_DOC_NUMField;
+
+        private byte nP_PDTField;
+
+        /// <remarks/>
+        public string WIC_NUM
+        {
+            get
+            {
+                return this.wIC_NUMField;
+            }
+            set
+            {
+                this.wIC_NUMField = value;
+            }
+        }
+
+        /// <remarks/>
+        public uint WIC_CASE_NUM
+        {
+            get
+            {
+                return this.wIC_CASE_NUMField;
+            }
+            set
+            {
+                this.wIC_CASE_NUMField = value;
+            }
+        }
+
+        /// <remarks/>
+        public System.DateTime WIC_DT_BEGIN
+        {
+            get
+            {
+                return this.wIC_DT_BEGINField;
+            }
+            set
+            {
+                this.wIC_DT_BEGINField = value;
+            }
+        }
+
+        /// <remarks/>
+        public System.DateTime WIC_DT_END
+        {
+            get
+            {
+                return this.wIC_DT_ENDField;
+            }
+            set
+            {
+                this.wIC_DT_ENDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string WIC_STATUS
+        {
+            get
+            {
+                return this.wIC_STATUSField;
+            }
+            set
+            {
+                this.wIC_STATUSField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte WIC_CD
+        {
+            get
+            {
+                return this.wIC_CDField;
+            }
+            set
+            {
+                this.wIC_CDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string WIC_CD_Name
+        {
+            get
+            {
+                return this.wIC_CD_NameField;
+            }
+            set
+            {
+                this.wIC_CD_NameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public bool SIGN_ANLK_NARKOTIK_INTOXICATION
+        {
+            get
+            {
+                return this.sIGN_ANLK_NARKOTIK_INTOXICATIONField;
+            }
+            set
+            {
+                this.sIGN_ANLK_NARKOTIK_INTOXICATIONField = value;
+            }
+        }
+
+        /// <remarks/>
+        public bool VIOLATION_EXTENSION
+        {
+            get
+            {
+                return this.vIOLATION_EXTENSIONField;
+            }
+            set
+            {
+                this.vIOLATION_EXTENSIONField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string HOSPITAL_NAME
+        {
+            get
+            {
+                return this.hOSPITAL_NAMEField;
+            }
+            set
+            {
+                this.hOSPITAL_NAMEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public uint HOSPITAL_CODE
+        {
+            get
+            {
+                return this.hOSPITAL_CODEField;
+            }
+            set
+            {
+                this.hOSPITAL_CODEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string NP_SURNAME
+        {
+            get
+            {
+                return this.nP_SURNAMEField;
+            }
+            set
+            {
+                this.nP_SURNAMEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string NP_NAME
+        {
+            get
+            {
+                return this.nP_NAMEField;
+            }
+            set
+            {
+                this.nP_NAMEField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string NP_PATRONYMIC
+        {
+            get
+            {
+                return this.nP_PATRONYMICField;
+            }
+            set
+            {
+                this.nP_PATRONYMICField = value;
+            }
+        }
+
+        /// <remarks/>
+        public uint NP_NUMIDENT
+        {
+            get
+            {
+                return this.nP_NUMIDENTField;
+            }
+            set
+            {
+                this.nP_NUMIDENTField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string NP_DOC_NUM
+        {
+            get
+            {
+                return this.nP_DOC_NUMField;
+            }
+            set
+            {
+                this.nP_DOC_NUMField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte NP_PDT
+        {
+            get
+            {
+                return this.nP_PDTField;
+            }
+            set
+            {
+                this.nP_PDTField = value;
+            }
+        }
+    }
+
+    internal static class ParseHelpers
+    {
+
+        public static Stream ToStream(this string @this)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(@this);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public static T ParseXML<T>(this string @this) where T : class
+        {
+            var reader = XmlReader.Create(@this.Trim().ToStream(), new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Document });
+            return new XmlSerializer(typeof(T)).Deserialize(reader) as T;
+        }
+
+    }
+
+    public class Part : IEquatable<Part>
+    {
+        public int Tabn { get; set; }
+        public string Icnum { get; set; }
+        public string Ceh { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Part objAsPart = obj as Part;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public bool Equals(Part other)
+        {
+            if (other == null) return false;
+            return (this.Icnum.Equals(other.Icnum));
+        }
+        public override int GetHashCode()
+        {
+            return Tabn;
+        }
+    }
+}
